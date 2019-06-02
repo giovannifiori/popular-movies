698f900dd155f6ad71977b9276508c1dc9bdea7b
Giovanni Fiori <giovannicfiori@gmail.com>
2019-06-02 19:09:07 -0400

    Added refresh button and error message

--- app/src/main/java/com/example/android/popularmovies/views/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/views/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.android.popularmovies.BuildConfig;
 import com.example.android.popularmovies.R;
@@ -30,7 +31,9 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Pos
     public static final String MOVIE_EXTRA = "com.example.android.popularmovies_movie";
     private static final String SORT_MOST_POPULAR = "popular";
     private static final String SORT_TOP_RATED = "top_rated";
+    private String mLastSortedBy = null;
     private ProgressBar mLoadingIndicator;
+    private TextView mErrorMessageTextView;
     private RecyclerView mRecyclerView;
     private MoviesAdapter mMoviesAdapter;
     private IMovieService mMovieService;
@@ -49,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Pos
         mRecyclerView.setLayoutManager(layoutManager);
         mMoviesAdapter = new MoviesAdapter(this);
         mRecyclerView.setAdapter(mMoviesAdapter);
+        mErrorMessageTextView = findViewById(R.id.tv_error_message);
 
         fetchMovies(SORT_MOST_POPULAR);
     }
@@ -63,34 +67,55 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Pos
     public boolean onOptionsItemSelected(MenuItem item) {
         int selectedId = item.getItemId();
 
-        if (selectedId == R.id.action_popularity) {
-            fetchMovies(SORT_MOST_POPULAR);
-        } else if (selectedId == R.id.action_top_rated) {
-            fetchMovies(SORT_TOP_RATED);
+        switch (selectedId) {
+            case R.id.action_refresh:
+                fetchMovies(mLastSortedBy);
+                break;
+            case R.id.action_popularity:
+                fetchMovies(SORT_MOST_POPULAR);
+                break;
+            case R.id.action_top_rated:
+                fetchMovies(SORT_TOP_RATED);
+                break;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void fetchMovies(@NonNull String sortType) {
-        mLoadingIndicator.setVisibility(View.VISIBLE);
+    private void showErrorMessage() {
         mRecyclerView.setVisibility(View.INVISIBLE);
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showContent() {
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void fetchMovies(@NonNull String sortType) {
+        mLastSortedBy = sortType;
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mLoadingIndicator.setVisibility(View.VISIBLE);
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+
         mMovieService.getMovies(sortType, BuildConfig.ApiKey).enqueue(new Callback<MovieAPIResponse>() {
             @Override
             public void onResponse(Call<MovieAPIResponse> call, Response<MovieAPIResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     mMoviesAdapter.setMoviesData(response.body().getMovies());
-                    mLoadingIndicator.setVisibility(View.INVISIBLE);
-                    mRecyclerView.setVisibility(View.VISIBLE);
+                    showContent();
                     mRecyclerView.smoothScrollToPosition(0);
                 } else {
                     Log.d(TAG, "onResponse: with error");
+                    showErrorMessage();
                 }
             }
 
             @Override
             public void onFailure(Call<MovieAPIResponse> call, Throwable t) {
-                mLoadingIndicator.setVisibility(View.INVISIBLE);
+                showErrorMessage();
                 Log.d(TAG, "onFailure: ERROR" + t);
             }
         });
